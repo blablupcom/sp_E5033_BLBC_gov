3c249bbf14ff49cffecfb715a1cf2781c7c7fe4c
blablupcom <o.volodin@yahoo.com>
2015-08-28 14:15:17 +0300

    added lxml to bs

--- scraper.py
+++ scraper.py
@@ -20,7 +20,7 @@ def convert_mth_strings ( mth_string ):
 
 # pull down the content from the webpage
 html = urllib2.urlopen(url)
-soup = BeautifulSoup(html)
+soup = BeautifulSoup(html, 'lxml')
 
 # find all entries with the required class
 blocks = soup.findAll('h2', {'class':'removeLowerMargin'})
